1574a2075dc2a85f1a7d2cbeb6198d8dd3e560f2
Adam Smith and Kevin Boyle <adam@>
2018-01-02 11:53:48 -0800

    add final gulp tasks, ie clean & build

--- build/js/app.js
+++ build/js/app.js
@@ -1,29 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var Entry = require('./../js/entry.js').entryModule;
-
-var journalEntry = [];
-
-$(document).ready(function(){
-  $("#entry-form").submit(function(event){
-    event.preventDefault();
-
-    var journalEntryTitle = $('#entryTitle').val();
-    var journalEntryBody = $('#entryBody').val();
-    var newEntry = new Entry(journalEntryTitle, journalEntryBody);
-
-    $('#entry-output').append('<div class="journal-entry">' +
-                                '<div class="entry-title">' + newEntry.title + '</div>' +
-                                '<div class="entry-body">' + newEntry.body + '</div>' +
-                                '<div class="word-count">This entry has ' + newEntry.wordCount() + ' words.</div>' +
-                                '<div class="vowel-count">This entry has ' + newEntry.vowels() + ' vowels.</div>' +
-                                '<div class="consonant-count">This entry has ' + newEntry.consonants() + ' consonants.</div>' +
-                                '<div class="teaser">' + newEntry.getTeaser() + '</div> <br>' +
-                              '</div>'
-                            );
-  });
-});
-
-},{"./../js/entry.js":2}],2:[function(require,module,exports){
 
 
 function Entry(title, body) {
@@ -88,4 +63,29 @@ Entry.prototype.getTeaser = function(){
 
 exports.entryModule = Entry;
 
-},{}]},{},[1]);
+},{}],2:[function(require,module,exports){
+var Entry = require('./../js/entry.js').entryModule;
+
+var journalEntry = [];
+
+$(document).ready(function(){
+  $("#entry-form").submit(function(event){
+    event.preventDefault();
+
+    var journalEntryTitle = $('#entryTitle').val();
+    var journalEntryBody = $('#entryBody').val();
+    var newEntry = new Entry(journalEntryTitle, journalEntryBody);
+
+    $('#entry-output').append('<div class="journal-entry">' +
+                                '<div class="entry-title">' + newEntry.title + '</div>' +
+                                '<div class="entry-body">' + newEntry.body + '</div>' +
+                                '<div class="word-count">This entry has ' + newEntry.wordCount() + ' words.</div>' +
+                                '<div class="vowel-count">This entry has ' + newEntry.vowels() + ' vowels.</div>' +
+                                '<div class="consonant-count">This entry has ' + newEntry.consonants() + ' consonants.</div>' +
+                                '<div class="teaser">' + newEntry.getTeaser() + '</div> <br>' +
+                              '</div>'
+                            );
+  });
+});
+
+},{"./../js/entry.js":1}]},{},[2]);
